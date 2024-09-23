020e26b2e82e9766c8e02b9341761d2d17dbf988
Agustin-Islas <agustinislas44@hotmail.com>
2024-09-23 20:05:08 -0300

    add

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MiCompTecnologia from "./MiCompTecnologia";
+
+import "./index.css";
+
+const App = () => (
+  <div className="container">
+     <MiCompTecnologia />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
+
+  
---
+++ src/MiCompTecnologia.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function MiCompTecnologia() {
+    // return a jsx ul list of a menu with five random items
+    return <div>
+       <p> Esta es una noticia Tecnológica</p>
+
+        <p> ¡Apple en banca rota! </p>
+
+    </div>;
+}
+
+export default MiCompTecnologia
+
